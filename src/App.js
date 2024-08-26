ecf6afccdafd0604f97c0566ef400444f6bfaa8f
tajche77 <bozic.tamara@gmail.com>
2024-08-26 14:14:19 +0200

    added Footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        coded by
+        <a
+          href="https://github.com/tajche77/react-weather-application"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          TamaraB
+        </a>
+        , and hosted on {""}
+        <a
+          href="https://kaleidoscopic-toffee-53c4a6.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
