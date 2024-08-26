339a2a235bbe2273d751f35333e561b5db08b82d
tajche77 <bozic.tamara@gmail.com>
2024-08-26 14:33:37 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        coded by
-        <a
-          href="https://github.com/tajche77/react-weather-application"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          TamaraB
-        </a>
-        , and hosted on {""}
-        <a
-          href="https://kaleidoscopic-toffee-53c4a6.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          coded by
+          <a
+            href="https://github.com/tajche77/react-weather-application"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            TamaraB
+          </a>
+          , and hosted on {""}
+          <a
+            href="https://kaleidoscopic-toffee-53c4a6.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
